52b5e38dcac30c403cf1e20d1e71a0d89c1715b9
ArsenyVekshin <arsvek87@gmail.com>
2022-03-12 00:10:44 +0500

    rotate bodies debug

--- main.py
+++ main.py
@@ -4,61 +4,62 @@ import os.path
 import math as m
 
 cmdfile = "config.txt"
+imgfile = "view2d.jpg"
 filename2d = "map2d.txt"
-filename3d = "map2d.txt"
+filename3d = "map3d.txt"
+picX, picY = 512, 512
 
 # region file and array init
-if os.path.isfile(filename2d): #check existing 2dmap file
-    pic = np.genfromtxt(filename2d,dtype=np.int8)
-else:
-    pic = np.zeros((1024, 1024, 3), dtype=np.int8)
+#if os.path.isfile(filename2d): #check existing 2dmap file
+#    pic = np.genfromtxt(filename2d,dtype=np.uint8)
+#else:
+pic = np.zeros((picY, picX, 3), dtype=np.uint8)
 
-if os.path.isfile(filename3d):
-    zpic = np.genfromtxt(filename3d) #check existing 3dmap file
-else:
-    zpic = np.zeros((1024, 1024))
+#if os.path.isfile(filename3d):
+#    zpic = np.genfromtxt(filename3d) #check existing 3dmap file
+#else:
+zpic = np.zeros((picY, picX), dtype=int)
 
 if not os.path.isfile(cmdfile): #check existing of cmdfile
     sys.quit()
-
-cmd = open(cmdfile).readline() #check is cmd enaged
-if cmd == "":
-    sys.quit()
+print("prev: ok")
 
 # endregion
-arr=list(cmd.split(" "))
-arr=[arr[0], int(arr[1]), int(arr[2]), int(arr[3]), int(arr[4]), int(arr[5]), int(arr[6]), int(arr[7]), int(arr[8])]
+
 #region generation functions init
 def Draw_circle(clr, c_point, R, flag=0):
-    x_min, x_max = c_point[0]-R, c_point[0]+R+1
-    y_min, y_max = c_point[1]-R, c_point[1]+R+1
+    x_min, x_max = c_point[0]-R, c_point[0]+1 + R
+    y_min, y_max = c_point[1]-R, c_point[1]+1 + R
 
     for y in range(y_min, y_max):
         for x in range(x_min, x_max):
-            if(m.sqrt((x-c_point[0])**2 + (y-c_point[1])**2) <= R):
-                if(flag==1 or pic[y][x]==[0,0,0] or clr==[0,0,0]):
-                    pic[y][x]=clr
-                    zpic[y][x]=int(c_point[2])
+            if(((x-c_point[0])**2 + (y-c_point[1])**2) <= R**2):
+                if(pic[y][x][0]==clr[0] and pic[y][x][1]==clr[1] and pic[y][x][2]==clr[2]): continue
+                pic[y][x]=clr
+                zpic[y][x]=int(c_point[2])
 
 def Draw_cone(arr):
     #cone r g b x0 y0 z0 R H
-    clr = arr[1:3]
-    c_point = arr[4:6]
-    H, R = arr[7], arr[8]
+    clr = arr[1:4]
+    clr.reverse()
+    c_point = arr[4:7]
+    R, H = arr[7], arr[8]
     dh = (H - c_point[2])/R #шаг по высоте на изменении радиуса на 1
-
-    Draw_circle([0,0,0], c_point, R) #создаем "дырку"
+    R=1
+    #Draw_circle([250,250,250], c_point, R) #создаем "дырку"
 
     c_point[2]+=H
-    for i in range(arr[8]):
-        R-=1
+    for i in range(arr[7]):
+        R+=1
         c_point[2]-=dh
         Draw_circle(clr, c_point, int(R))
 
 def Draw_cylinder(arr):
     #cylinder r g b x0 y0 z0 R H
-    clr = arr[1:3]
-    c_point = arr[4:6]
+    clr = arr[1:4]
+    clr.reverse()
+    c_point = arr[4:7]
+
     H, R = arr[7], arr[8]
     c_point[2] += H
 
@@ -66,19 +67,19 @@ def Draw_cylinder(arr):
 
 def Draw_sphere(arr):
     #sphere r g b x0 y0 z0 R
-    clr = arr[1:3]
-    c_point = arr[4:6]
+    clr = arr[1:4]
+    clr.reverse()
+    c_point = arr[4:7]
     R = arr[7]
-    Draw_circle([0,0,0], c_point, R) #создаем "дырку"
 
     c_point[2]+=int(R/2)
 
-    x_min, x_max = c_point[0] - R, c_point[0] + R + 1
-    y_min, y_max = c_point[1] - R, c_point[1] + R + 1
+    x_min, x_max = c_point[0] - R, c_point[0] + R+1
+    y_min, y_max = c_point[1] - R, c_point[1] + R+1
 
     for y in range(y_min, y_max):
         for x in range(x_min, x_max):
-            if (m.sqrt((x - c_point[0]) ** 2 + (y - c_point[1]) ** 2) <= R):
+            if (((x - c_point[0]) ** 2 + (y - c_point[1]) ** 2) <= R**2):
                 pic[y][x] = clr
                 zpic[y][x] = int(m.sqrt(R**2 - (x - c_point[0]) ** 2 - (y - c_point[1]) ** 2))
 
@@ -87,32 +88,54 @@ def Draw_sphere(arr):
 #endregion
 
 #region cmd reaction
-if arr[0]=="cylinder":
-    Draw_cylinder(arr)
+f = open(cmdfile)
 
-elif arr[0]=="cone":
-    Draw_cone(arr)
+for s in f:
+    if(s==""): continue
+    arr=list(s.split(" "))
+    print(arr)
+    for i in range(1,len(arr)): arr[i]=int(arr[i])
 
-elif arr[0]=="sphere":
-    Draw_sphere(arr)
+    if arr[0]=="cylinder":
+        print("step1")
+        Draw_cylinder(arr)
+        print("step2")
+    elif arr[0]=="cone":
+        Draw_cone(arr)
 
-elif arr[0]=="pyramid":
-    print("pyramid generation")
+    elif arr[0]=="sphere":
+        Draw_sphere(arr)
 
-elif arr[0] == "pyramid":
-    print("pyramid generation")
+    elif arr[0]=="pyramid":
+        print("pyramid generation")
 
-elif arr[0] == "2dpic":
-    print("2dpic visualise")
+    elif arr[0] == "prism":
+        print("prism generation")
 
-elif arr[0] == "3dpic":
-    print("3dpic visualise")
+    elif arr[0] == "2dpic":
+        print("2dpic visualise")
+
+    elif arr[0] == "3dpic":
+        print("3dpic visualise")
 #endregion
-
+cv2.imwrite(imgfile, pic)
 #region save_data
-np.save(filename2d, pic)
-np.save(filename3d, zpic)
+def save2files():
+    f1 = open(filename2d, "w")
+    f2 = open(filename3d, "w")
+    for y in range(picY):
+        for x in range(picX):
+            f1.write(str(pic[y][x][0]) + " ")
+            f1.write(str(pic[y][x][1]) + " ")
+            f1.write(str(pic[y][x][2]) + " ")
+            f2.write(str(zpic[y][x]) + " ")
+        f1.write("\n")
+        f2.write("\n")
+    f1.close()
+    f2.close()
+save2files()
 #endregion
+
 
 """
 Словарь входных данных для config.txt
