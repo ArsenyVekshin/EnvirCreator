1944771e000f59b038e2cfa49a7ef764c5cb97f8
ArsenyVekshin <arsvek87@gmail.com>
2022-03-11 20:36:56 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,65 @@
+import numpy as np
+import cv2
+import os.path
+
+cmdfile = "config.txt"
+filename2d = "map2d.txt"
+filename3d = "map2d.txt"
+
+# region file and array init
+if os.path.isfile(filename2d): #check existing 2dmap file
+    pic = np.genfromtxt(dtype=np.int8)
+else:
+    pic = np.zeros((1024, 1024, 3), dtype=np.int8)
+
+if os.path.isfile(filename2d):
+    zpic = np.genfromtxt(dtype=np.float64) #check existing 3dmap file
+else:
+    zpic = np.zeros((1024, 1024), dtype=np.float64)
+
+if not os.path.isfile(cmdfile): #check existing of cmdfile
+    sys.quit()
+
+cmd = open(cmdfile).readline() #check is cmd enaged
+if cmd == "":
+    sys.quit()
+
+# endregion
+
+arr=list(cmd.split(" "))
+
+#region generation functions init
+
+#endregion
+
+#region cmd reaction
+if arr[0]=="cylinder":
+    print("cylinder generation")
+
+elif arr[0]=="cone":
+    print("cone generation")
+
+elif arr[0]=="sphere":
+    print("sphere generation")
+
+elif arr[0]=="pyramid":
+    print("pyramid generation")
+
+elif arr[0] == "pyramid":
+    print("pyramid generation")
+
+elif arr[0] == "2dpic":
+    print("2dpic visualise")
+
+elif arr[0] == "3dpic":
+    print("3dpic visualise")
+#endregion
+
+#region save_data
+np.save(filename2d, pic)
+np.save(filename3d, zpic)
+#endregion
+
 """
 Словарь входных данных для config.txt
 
@@ -22,7 +84,3 @@
 		map2d.txt(картинка 2д)
 		map3d.txt(карта высот)				
 """
-
-file= open("config11.txt", "w")
-file.write("TEST")
-file.close()
